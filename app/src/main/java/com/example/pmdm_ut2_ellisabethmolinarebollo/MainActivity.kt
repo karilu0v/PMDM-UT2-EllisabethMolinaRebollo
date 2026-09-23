dce5a6b321d1a5d0a77d340a0b23c289d7d0daef
eli <fokko02love@gmail.com>
2026-09-23 13:25:38 +0200

    texto con parámetro de entrada

---
+++ app/src/main/java/com/example/pmdm_ut2_ellisabethmolinarebollo/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.pmdm_ut2_ellisabethmolinarebollo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pmdm_ut2_ellisabethmolinarebollo.ui.theme.PMDMUT2EllisabethMolinaRebolloTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PMDMUT2EllisabethMolinaRebolloTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Titulo(texto = ":OOOOOOOOO")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PMDMUT2EllisabethMolinaRebolloTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Titulo(texto: String) {
+    Text(text = texto)
+}
